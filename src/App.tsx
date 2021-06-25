3188b0101a2f5ec31228804d83396dac3485ffc0
Mihai N <74415486+devneagu@users.noreply.github.com>
2021-06-25 22:39:11 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./styles.css";
+
+function FitnessJournal() {
+  return <p>FitnessJournal</p>;
+}
+
+export default function App() {
+  return (
+    <div className="App">
+      <FitnessJournal />
+    </div>
+  );
+}
